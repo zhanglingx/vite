5d2259918f1b5e3b22c095674b2bf950e00e2a1a
Ling Z <771152260@qq.com>
2021-06-29 19:25:43 +0800

    docs(vite.js): comment

--- packages/vite/bin/vite.js
+++ packages/vite/bin/vite.js
@@ -1,21 +1,27 @@
 #!/usr/bin/env node
-
+// 判断：是否是开发环境
 if (!__dirname.includes('node_modules')) {
   try {
     // only available as dev dependency
+    // 如果是开发环境，尝试引入source-map-support???并install
     require('source-map-support').install()
   } catch (e) {}
 }
 
+// 全局变量声明：vite开始执行时间
 global.__vite_start_time = Date.now()
 
 // check debug mode first before requiring the CLI.
+// 变量声明：debugIndex = -d/--debug 在传入的环境变量Array的索引，不用indexOf的原因是要判断两个
+// 变量声明：filterIndex = -f/--filter 在传入的环境变量Array的索引
 const debugIndex = process.argv.findIndex((arg) => /^(?:-d|--debug)$/.test(arg))
 const filterIndex = process.argv.findIndex((arg) =>
   /^(?:-f|--filter)$/.test(arg)
 )
+// 变量声明：profileIndex = -f/--profile 在传入的环境变量Array的索引
 const profileIndex = process.argv.indexOf('--profile')
 
+// 当 debugIndex 存在，则???
 if (debugIndex > 0) {
   let value = process.argv[debugIndex + 1]
   if (!value || value.startsWith('-')) {
@@ -28,7 +34,7 @@ if (debugIndex > 0) {
       .join(',')
   }
   process.env.DEBUG = value
-
+  // 且当 filterIndex 存在，则???
   if (filterIndex > 0) {
     const filter = process.argv[filterIndex + 1]
     if (filter && !filter.startsWith('-')) {
@@ -54,5 +60,6 @@ if (profileIndex > 0) {
     session.post('Profiler.start', start)
   })
 } else {
+  // 装载node-cli
   start()
 }
